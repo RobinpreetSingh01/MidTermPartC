1e5b0965f5c3910a6e89fe9cfe5b0db80edbe8e8
RobinpreetSingh01 <160060975+RobinpreetSingh01@users.noreply.github.com>
2024-02-21 14:08:07 -0500

    Update UserProfile.java

--- src/midtermexam_versiona_extensioncode/UserProfile.java
+++ src/midtermexam_versiona_extensioncode/UserProfile.java
@@ -19,7 +19,7 @@ package midtermexam_versiona_extensioncode;
 public class UserProfile 
 {
     
-    private String userID;//the userID
+    private String userID;
     private String genre;// the user's preferred genre of movie
     private String[] genres = {"Comedy", "Drama", "Action", "Mystery"};//available genres of movies
     
